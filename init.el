6486010b39e48cc141a0c219ede02bf8d402389a
Dimitar Petrov <petrov.dimp@gmail.com>
2016-05-29 23:49:06 +0300

    Removed hardcoded path from init.el and cleaned default theme from init.el

--- init.el
+++ init.el
@@ -1,18 +1,3 @@
 (package-initialize)
 (require 'ob-tangle)
-(org-babel-load-file "/home/dpetrov/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (cyberpunk)))
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "6df30cfb75df80e5808ac1557d5cc728746c8dbc9bc726de35b15180fa6e0ad9" "7b4d9b8a6ada8e24ac9eecd057093b0572d7008dbd912328231d0cada776065a" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file "~/.emacs.d/config.org")
