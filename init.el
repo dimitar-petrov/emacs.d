247291264b017eb9ee2cf7786388a60a81b3803a
Dimitar Petrov <petrov.dimp@gmail.com>
2016-05-31 08:01:36 +0300

    The following additions have been made to the config.org file * Keybindings for ogr-store-link, org-agenda, org-capture and org-iswitchb * Default files for org-mode: gtd.org, notes.org, journal.org
    And init.el has been cleaned from the default template

--- init.el
+++ init.el
@@ -1,17 +1,3 @@
 (package-initialize)
 (require 'ob-tangle)
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
